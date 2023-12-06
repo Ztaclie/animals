5a473f9e3c257829ec65176798648fc6a6facf90
Ztaclie <melmeh42@gmail.com>
2023-12-06 13:09:31 +0300

    added func for events and looked results

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import AnimalShow from "./components/AnimalShow";
 
 function App() {
-    return (<div className="App">App
+    const handleClick = () => {
+        console.log("clicked");
+    };
+    const handleMouseMove = () => {
+        console.log("Hovered");
+    };
+
+    return (<div className="App">
+        <h1>Animal Ranch App</h1>
+        <button type="button" onClick={handleClick}>Add Animal</button>
+        <button type="button" onMouseMove={handleMouseMove}>Hover Animal</button>
     <AnimalShow />
     </div>);
 };
