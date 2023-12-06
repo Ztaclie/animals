5445446e023b594b923fabe4db08b83a0b63d5cb
Ztaclie <melmeh42@gmail.com>
2023-12-06 13:36:02 +0300

    learned map feature and using svg stuff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import {useState} from "react";
+import { useState } from "react";
 import AnimalShow from "./components/AnimalShow";
 
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-    const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
-    const handleClick = () => {
-        setCount(count + 1);
-    };
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
 
-    return (<div className="App">
-        <h1>Animal Ranch App</h1>
-        <button type="button" onClick={handleClick}>Add Animal</button>
-        <div>Animal Count: {count}</div>
-    <AnimalShow />
-    </div>);
-};
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
+  return (
+    <div className="App">
+      <h1>Animal Ranch App</h1>
+      <button type="button" onClick={handleClick}>
+        Add Animal
+      </button>
+      {renderedAnimals}
+    </div>
+  );
+}
 
 export default App;
